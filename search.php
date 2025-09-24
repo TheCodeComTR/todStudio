d92650e5554aaf1db1e90892dde37fe15bccf60b
TheCodeEfe <efe@thecode.com.tr>
2025-09-24 14:57:54 +0300

    search

--- search.php
+++ search.php
@@ -26,7 +26,57 @@ view('header/header');
     <section class="search-results-list">
         <div class="container">
             <div class="results-list">
-                <!-- Sonuçlar JavaScript ile dinamik olarak yüklenecek -->
+                
+                <div class="result-item">
+                    <h3>TELLUS EGET ODIO</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>MASSA ALIQUAM</h3>
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>JUSTO DIAM</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>NUNC VELIT</h3>
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>CONSECTETUR ADIPISCING</h3>
+                    <p>Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>ELIT SED DO</h3>
+                    <p>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>EIUSMOD TEMPOR</h3>
+                    <p>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>INCIDIDUNT UT</h3>
+                    <p>Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>LABORE ET DOLORE</h3>
+                    <p>Magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            
+                <div class="result-item">
+                    <h3>MAGNA ALIQUA</h3>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+                </div>
+            
             </div>
 
             <!-- Pagination -->
